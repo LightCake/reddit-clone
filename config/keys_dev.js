52581c4789235003588f97ea9862d79be1249994
Jon Wee Joey Liew <joeyjon@protonmail.com>
2019-10-08 20:04:54 +0200

    refactor and restructure

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  PGUSER: "me",
-  PGHOST: "localhost",
-  PGDATABASE: "reddit",
-  PGPASSWORD: "password",
-  PGPORT: 5432,
-  secretOrKey: "HpfuiJF6pZ"
-};
--- validation/valid-text.js
+++
@@ -1,6 +0,0 @@
-// Check whether the value is a string with a length greater than 0
-const validText = str => {
-  return typeof str === "string" && str.trim().length > 0;
-};
-
-module.exports = validText;
